0773f653aa3e84dd3586960923d83ea9203840b0
Daniel Diprose <daniel.diprose@gmail.com>
2019-03-01 15:45:19 +1100

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ var path = require('path');
 
 module.exports = {
   entry: './src/app.js',
+  devServer: {
+    writeToDisk: true,
+    contentBase: path.join(__dirname, './'),
+    compress: true,
+    port: 9000
+  },
   module: {
     rules: [
       // {
